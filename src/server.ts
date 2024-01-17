471272ac4424dcceec51f40ab0a29187707a051e
Narutchai <koonfluk2015@gmail.com>
2024-01-17 20:54:44 +0700

    add mysql

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
+import mysql from 'mysql2/promise';
 
 config();
+
+
+let conn = null as any ;
+
+const connect = async () => {
+  conn = await mysql.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME,
+    port: Number(process.env.DBPORT),
+  });
+};
+
 
 const app = express();
 app.use(cors());
@@ -14,7 +29,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/getsurveyor', async (req, res) => {
+    const result:any = await conn?.query('SELECT * FROM Surveyor');
+    res.send(result[0]);
+});
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await connect();
   console.log(`server on http://localhost:${PORT}`);
 });
